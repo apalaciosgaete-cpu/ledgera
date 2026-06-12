46e4b188faee15d6ab795510d3461810751d0e97
apalaciosgaete-cpu <apalaciosgaete@gmail.com>
2026-06-12 12:04:35 -0400

    Show billing status panel in settings

--- src/app/(protected)/configuracion/facturacion/page.tsx
+++ src/app/(protected)/configuracion/facturacion/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { BillingCheckoutStatusBanner } from "@/components/billing/BillingCheckoutStatusBanner";
+import { BillingStatusPanel } from "@/components/billing/BillingStatusPanel";
 import ConfiguracionShell from "../ConfiguracionShell";
 
 export default function FacturacionPage() {
   return (
     <>
       <BillingCheckoutStatusBanner />
+      <BillingStatusPanel />
       <ConfiguracionShell forcedSection="facturacion" />
     </>
   );
