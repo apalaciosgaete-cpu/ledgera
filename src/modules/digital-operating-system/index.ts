454d03bece70421326701980d8fb9843e9793fa2
apalaciosgaete-cpu <apalaciosgaete@gmail.com>
2026-06-19 07:59:29 -0400

    feat: export digital operating summary

--- src/modules/digital-operating-system/index.ts
+++ src/modules/digital-operating-system/index.ts
@@ -1,4 +1,5 @@
 export * from "./domain/types";
 export * from "./domain/modules";
 export * from "./application/buildDigitalOperatingContext";
+export * from "./application/deriveOperatingSummary";
 export * from "./infrastructure/digitalOperatingRepository";
