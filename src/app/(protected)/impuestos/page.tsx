ee75a5d217ea5580d8a74109cc883422b77c9413
apalaciosgaete-cpu <apalaciosgaete@gmail.com>
2026-07-03 21:18:30 -0400

    chore: remove legacy shim

--- src/app/(protected)/impuestos/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export default function ImpuestosRedirectPage() {
-  useEffect(() => { window.location.href = "/experto/tributario"; }, []);
-  return null;
-}
