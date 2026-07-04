134d76eb00938f8b5716be7fe3b49c98e7a8bb03
apalaciosgaete-cpu <apalaciosgaete@gmail.com>
2026-07-03 21:16:48 -0400

    chore: remove legacy shim

--- src/app/(protected)/auditoria/integridad/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export default function RedirectPage() {
-  useEffect(() => { window.location.href = "/experto/auditoria"; }, []);
-  return null;
-}
