45c64919c798e7f11182760453a38d27e5e59d69
apalaciosgaete-cpu <apalaciosgaete@gmail.com>
2026-07-03 21:11:49 -0400

    chore: remove orphan browser asset

--- public/sw.js
+++
@@ -1,6 +0,0 @@
-// LEDGERA service worker retired.
-// Kept intentionally as a no-op file so older browser registrations stop receiving chat-specific handlers.
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.registration.unregister());
-});
