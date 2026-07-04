b1d06768e40ae90de93efeaf27f19ef93378cd71
apalaciosgaete-cpu <apalaciosgaete@gmail.com>
2026-07-03 21:17:13 -0400

    chore: remove legacy shim

--- src/app/(protected)/impuestos/evidencia/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useEffect } from "react";
-
-export default function RedirectPage() {
-  useEffect(() => { window.location.href = "/experto/verificaciones"; }, []);
-  return null;
-}
